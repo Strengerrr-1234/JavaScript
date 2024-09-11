7d21e71a9bb31979ff1d2eba5ec233b1a3c4267c
AAYUSH  SHAKYAWAR <147983388+Strengerrr-1234@users.noreply.github.com>
2024-09-11 04:50:48 -0400

    Update _1_introduction_of_javascript.js

---
+++ Learning_Javascript/_1_Introduction of javascript/_1_introduction_of_javascript.js
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Introduction to JavaScript</title>
+</head>
+
+<body>
+    <script>
+     /*
+        What is JavaScript
+
+        JavaScript (js) is a light-weight object-oriented programming language which is used by several websites for scripting the 
+        webpages. It is an interpreted, full-fledged programming language that enables dynamic interactivity on websites when 
+        applied to an HTML document. It was introduced in the year 1995 for adding programs to the webpages in the Netscape 
+        Navigator browser. Since then, it has been adopted by all other graphical web browsers. With JavaScript, users can build
+        modern web applications to interact directly without reloading the page every time. The traditional website uses js to provide
+        several forms of interactivity and simplicity.
+
+        Application of JavaScript ----->
+           JavaScript is used to create interactive websites. It is mainly used for:
+           
+              1.Client-side validation,
+              2.Dynamic drop-down menus,
+              3.Displaying date and time,
+              4.Displaying pop-up windows and dialog boxes (like an alert dialog box, confirm dialog box and prompt dialog box),
+              5.Displaying clocks etc.
+
+      */
+    </script>
+</body>
+
+</html>
